661097939deac2e3bd6cf854e3c9e4a64548fcfe
GoEun Jeong <gogo8272@gmail.com>
2022-03-18 14:08:35 +0900

    [Add] UIFrame Constraints

---
+++ Flower-iOS/View/Constraints/UIFrame.swift
@@ -0,0 +1,13 @@
+//
+//  UIFrame.swift
+//  Flower-iOS
+//
+//  Created by GoEun Jeong on 2022/03/18.
+//
+
+import UIKit
+
+public struct UIFrame {
+    public static let width = UIScreen.main.bounds.width
+    public static let height = UIScreen.main.bounds.height
+}
