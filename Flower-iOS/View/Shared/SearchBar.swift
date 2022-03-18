6d007302c77e58d7b90a82db283478e4309f5d62
GoEun Jeong <gogo8272@gmail.com>
2022-03-18 14:07:40 +0900

    [Add] SearchBar

---
+++ Flower-iOS/View/Shared/SearchBar.swift
@@ -0,0 +1,42 @@
+//
+//  SearchBar.swift
+//  Flower-iOS
+//
+//  Created by GoEun Jeong on 2022/03/16.
+//
+
+import SwiftUI
+
+struct SearchBar: UIViewRepresentable {
+    @Binding var text: String
+    let placeholder: String
+
+    class Coordinator: NSObject, UISearchBarDelegate {
+
+        @Binding var text: String
+
+        init(text: Binding<String>) {
+            _text = text
+        }
+
+        func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            text = searchText
+        }
+    }
+
+    func makeCoordinator() -> SearchBar.Coordinator {
+        return Coordinator(text: $text)
+    }
+
+    func makeUIView(context: UIViewRepresentableContext<SearchBar>) -> UISearchBar {
+        let searchBar = UISearchBar(frame: .zero)
+        searchBar.delegate = context.coordinator
+        searchBar.searchBarStyle = .minimal
+        searchBar.placeholder = placeholder
+        return searchBar
+    }
+
+    func updateUIView(_ uiView: UISearchBar, context: UIViewRepresentableContext<SearchBar>) {
+        uiView.text = text
+    }
+}
