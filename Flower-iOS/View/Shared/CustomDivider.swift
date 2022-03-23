4e1124b49cf535b5d552c013b68a6854dbab78cf
GoEun Jeong <gogo8272@gmail.com>
2022-03-23 16:34:01 +0900

    [Add] CustomDivider

---
+++ Flower-iOS/View/Shared/CustomDivider.swift
@@ -0,0 +1,20 @@
+//
+//  CustomDivider.swift
+//  Flower-iOS
+//
+//  Created by GoEun Jeong on 2022/03/23.
+//
+
+import SwiftUI
+
+struct CustomDivider: View {
+    var body: some View {
+        Color.gray.frame(height: 1)
+    }
+}
+
+struct CustomDivider_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomDivider()
+    }
+}
