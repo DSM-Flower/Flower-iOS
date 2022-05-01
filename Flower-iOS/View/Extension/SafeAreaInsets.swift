89e5746aaac3d46d3460d205d23643840c17c5b8
GoEun Jeong <gogo8272@gmail.com>
2022-05-01 22:35:49 +0900

    [Add] SafeAreaInsets extension

---
+++ Flower-iOS/View/Extension/SafeAreaInsets.swift
@@ -0,0 +1,26 @@
+//
+//  SafeAreaInset.swift
+//  Flower-iOS
+//
+//  Created by GoEun Jeong on 2022/04/27.
+//
+
+import SwiftUI
+
+private struct SafeAreaInsetsKey: EnvironmentKey {
+    static var defaultValue: EdgeInsets {
+        (UIApplication.shared.windows.first(where: { $0.isKeyWindow })?.safeAreaInsets ?? .zero).insets
+    }
+}
+
+extension EnvironmentValues {
+    var safeAreaInsets: EdgeInsets {
+        self[SafeAreaInsetsKey.self]
+    }
+}
+
+private extension UIEdgeInsets {
+    var insets: EdgeInsets {
+        EdgeInsets(top: top, leading: left, bottom: bottom, trailing: right)
+    }
+}
