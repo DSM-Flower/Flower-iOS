f0fd234256be1c6fdb7d9ffdad788cadf0ad1eaf
GoEun Jeong <gogo8272@gmail.com>
2022-05-18 13:29:43 +0900

    [Add] FlowerImageView

---
+++ Flower-iOS/View/Shared/FlowerImageView.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  Flower-iOS
+//
+//  Created by GoEun Jeong on 2022/05/12.
+//
+
+import Foundation
+import SwiftUI
+import Kingfisher
+
+public struct FlowerImageView: View {
+    private let url: String
+    
+    public init(id: String) {
+        self.url = FlowerAPI.baseUrl + "/image?id=\(id)"
+    }
+    
+    public var body: some View {
+        Group {
+            KFImage(URL(string: url))
+                .placeholder { Image("loading") }
+                .resizable()
+                .cornerRadius(10)
+        }
+    }
+}
