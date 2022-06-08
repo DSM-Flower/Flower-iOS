eecb0c6e6375b3e3875714c81dee586db35497ba
GoEun Jeong <gogo8272@gmail.com>
2022-06-08 15:49:44 +0900

    [Modify] CommentsView

--- Flower-iOS/View/CommunityView/Comments/CommentsView.swift
+++ Flower-iOS/View/CommunityView/Comments/CommentsView.swift
@@ -9,42 +9,95 @@ import SwiftUI
 import Introspect
 
 struct CommentsView: View {
-    @ObservedObject var viewModel = CommentsViewModel()
+    @StateObject var viewModel = CommentsViewModel()
+    @State private var isDeleteAlert = false
+    @State private var isModifyAlert = false
+    @Environment(\.safeAreaInsets) private var safeAreaInsets
+    let postId: String
     
     var body: some View {
-        VStack {
-            ScrollView {
-                CommentRow()
+        ZStack {
+            VStack {
+                List {
+                    ForEach(viewModel.comments, id: \.id) { comment in
+                        CommentRow(comment: comment)
+                            .swipeActions {
+                                Button("수정") {
+                                    self.viewModel.selectedComment = comment
+                                    self.viewModel.modifiedComment = comment.content
+                                    self.isModifyAlert = true
+                                }.tint(.yellow)
+
+                                Button("삭제") {
+                                    self.viewModel.selectedComment = comment
+                                    self.isDeleteAlert = true
+                                }.tint(.red)
+                            }
+                    }
+                }.listStyle(.plain)
             }
-        }.navigationTitle("댓글")
-        .introspectTabBarController { (UITabBarController) in
-            UITabBarController.tabBar.isHidden = true
-        }
+            
+            VStack {
+                Spacer()
+                
+                VStack(spacing: 20) {
+                    HStack {
+                        UnderScoreTextField(placeholder: "닉네임", text: $viewModel.newNickname)
+                        
+                        UnderScoreSecureField(placeholder: "비밀번호", text: $viewModel.newPassword)
+                    }
+                    
+                    HStack {
+                        UnderScoreTextField(placeholder: "댓글을 입력해주세요.", text: $viewModel.newComment)
+                        
+                        Image(systemName: "paperplane.fill")
+                            .onTapGesture {
+                                viewModel.createComment()
+                            }
+                    }
+                    
+                }.padding()
+                    .padding(.bottom, safeAreaInsets.bottom)
+                    .background(Rectangle().foregroundColor(.white).shadow(radius: 5))
+            }
+        }.ignoresSafeArea(.all, edges: .bottom)
+            .textFieldAlert(isPresented: $isModifyAlert) { () -> TextFieldAlert in
+                TextFieldAlert(title: "댓글 수정", textFieldType: .double, action: viewModel.modifyComment, password: self.$viewModel.prevPassword, modifyString: self.$viewModel.modifiedComment)
+            }
+            .textFieldAlert(isPresented: $isDeleteAlert) { () -> TextFieldAlert in
+                TextFieldAlert(title: "댓글 삭제", textFieldType: .delete, action: viewModel.deleteComment, password: self.$viewModel.prevPassword, modifyString: self.$viewModel.modifiedComment)
+            }
+            .toast(message: viewModel.errorMessage,
+                   isShowing: $viewModel.showErrorToast)
+            .navigationTitle("댓글")
+            .introspectTabBarController { (UITabBarController) in
+                UITabBarController.tabBar.isHidden = true
+            }
+            .onAppear {
+                viewModel.onAppear(id: self.postId)
+            }
     }
 }
 
 struct CommentRow: View {
+    let comment: Comment
+    
     var body: some View {
         HStack {
-            VStack {
-                Text("닉네임")
+            VStack(alignment: .leading, spacing: 20) {
+                Text(comment.nickname)
                 
-                Text("댓글내용")
+                Text(comment.content)
+                    .font(.footnote)
             }
             
             Spacer()
-            
-            VStack {
-                // image
-                
-                Spacer()
-            }
-        }
+        }.padding(.vertical, 10)
     }
 }
 
 struct CommentsView_Previews: PreviewProvider {
     static var previews: some View {
-        CommentsView()
+        CommentsView(postId: "627b3ccacafd0109cb908f77")
     }
 }
--- Flower-iOS/View/CommunityView/Comments/CommentsViewModel.swift
+++ Flower-iOS/View/CommunityView/Comments/CommentsViewModel.swift
@@ -6,9 +6,109 @@
 //
 
 import Foundation
+import Combine
 
 class CommentsViewModel: ObservableObject {
+    @Published var comments = [Comment]()
+    
     @Published var newNickname = ""
     @Published var newPassword = ""
     @Published var newComment = ""
+    
+    @Published var prevPassword: String? = ""
+    @Published var modifiedComment: String? = ""
+    var selectedComment = Comment.EMPTY
+    
+    @Published var showErrorToast = false
+    @Published var errorMessage = ""
+    
+    private var postId = ""
+    
+    private let communityRepository: CommunityRepository
+    private let commentRepository: CommentRepository
+    
+    private let errorSubject = PassthroughSubject<CommentsError, Never>()
+    private var bag = Set<AnyCancellable>()
+    
+    public init(communityRepository: CommunityRepository = CommunityRepositoryImpl(), commentRepository: CommentRepository = CommentRepositoryImpl()) {
+        self.communityRepository = communityRepository
+        self.commentRepository = commentRepository
+        
+        errorSubject.sink(receiveValue: { error in
+            self.showErrorToast = true
+            
+            switch error {
+            case .noContent:
+                self.errorMessage = "내용을 작성해주세요."
+            case .notMatchedPassword:
+                self.errorMessage = "비밀번호가 일치하지 않습니다."
+            case .serverError:
+                self.errorMessage = "서버에 오류가 생겼습니다. 다시 시도해주세요."
+            }
+        }).store(in: &bag)
+    }
+    
+    public func onAppear(id: String) {
+        self.postId = id
+        
+        self.getComments()
+    }
+    
+    private func getComments() {
+        self.communityRepository.getDetailPost(id: self.postId, completion: {
+            self.comments = $0.comments
+        })
+    }
+    
+    public func createComment() {
+        if self.newComment == "" || self.newNickname == "" || self.newPassword == "" {
+            self.errorSubject.send(.noContent)
+        }
+        
+        self.commentRepository.createComment(postId: self.postId, content: self.newComment, nickname: self.newNickname, password: self.newPassword, completion: { result in
+            switch result {
+            case .success:
+                self.newComment = ""
+                self.newNickname = ""
+                self.newPassword = ""
+                
+                self.getComments()
+            case .failure:
+                self.errorSubject.send(.serverError)
+                break
+            }
+        })
+    }
+    
+    public func modifyComment() {
+        guard let prevPassword = prevPassword, let modifiedComment = modifiedComment else {
+            return
+        }
+        
+        self.commentRepository.modifyComment(id: self.selectedComment.id, content: modifiedComment, nickname: self.selectedComment.nickname, password: prevPassword, completion: {
+            switch $0 {
+            case .success:
+                self.onAppear(id: self.postId)
+            case let .failure(error: error):
+                self.showErrorToast = true
+                self.errorMessage = "비밀번호가 다릅니다."
+            }
+        })
+    }
+    
+    public func deleteComment() {
+        guard let prevPassword = prevPassword else {
+            return
+        }
+        
+        self.commentRepository.deleteComment(id: self.selectedComment.id, nickname: self.selectedComment.nickname, password: prevPassword, completion: {
+            switch $0 {
+            case .success:
+                self.onAppear(id: self.postId)
+            case let .failure(error: error):
+                self.showErrorToast = true
+                self.errorMessage = "비밀번호가 다릅니다."
+            }
+        })
+    }
 }
