ff3cb293eed452221d572187e2e33714397f1c74
GoEun Jeong <gogo8272@gmail.com>
2022-06-08 15:49:36 +0900

    [Modify] PostView

--- Flower-iOS/View/CommunityView/Post/PostView.swift
+++ Flower-iOS/View/CommunityView/Post/PostView.swift
@@ -9,35 +9,118 @@ import SwiftUI
 import Introspect
 
 struct PostView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
     @ObservedObject var viewModel = PostViewModel()
+    @State private var isDeleteAlert = false
+    @State private var isModifing = false
+    @State private var isModifyAlert = false
+    
+    let id: String
     
     var body: some View {
-        VStack {
-            RoundedRectangle(cornerRadius: 10)
-                .frame(width: UIFrame.width - 20, height: UIFrame.width - 20)
-            
-            Text("내용")
-            
-            Spacer()
-            
-            NavigationLink(destination: LazyView(CommentsView())) {
+        ZStack {
+            ScrollView {
                 HStack {
                     Spacer()
+                    Spacer()
                     
-                    Text("댓글 수")
-                        .foregroundColor(Color("Black"))
+                    VStack(alignment: .leading, spacing: 15) {
+                        ScrollView(.horizontal) {
+                            ForEach(viewModel.post.image, id: \.self) { image in
+                                FlowerImageView(id: image)
+                                    .frame(width: UIFrame.width - 30, height: UIFrame.width / 1.5)
+                            }
+                        }
+                        
+                        if isModifing {
+                            TextView(text: $viewModel.modifiedContent, placeholder: viewModel.post.content)
+                                .frame(height: UIFrame.height / 3.5)
+                                .background(RoundedRectangle(cornerRadius: 10).foregroundColor(Color("White")).shadow(color: .gray.opacity(0.5), radius: 2, x: 0, y: 4))
+                        } else {
+                            Text(viewModel.post.content)
+                        }
+                    }
+                    
+                    Spacer()
                 }
             }
-        }.navigationTitle("제목")
+            
+            VStack {
+                Spacer()
+                
+                if isModifing {
+                    ZStack(alignment: .center) {
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundColor(Color("Flower"))
+                            .frame(width: UIFrame.width - 30, height: 40)
+                        
+                        Text("수정하기")
+                            .foregroundColor(Color("White"))
+                    }.padding(.bottom, UIFrame.width / 9)
+                    .onTapGesture {
+                        self.isModifyAlert = true
+                        self.isModifing = false
+                    }
+                } else {
+                    HStack {
+                        Spacer()
+                        
+                        NavigationLink(destination: LazyView(CommentsView(postId: id))) {
+                            HStack(alignment: .top) {
+                                Text("\(viewModel.post.comments.count)")
+                                    .foregroundColor(Color("Black"))
+                                
+                                Image(systemName: "message")
+                            }.padding(.horizontal)
+                        }
+                    }.padding(.top, 10)
+                        .padding(.bottom, UIFrame.width / 9)
+                        .background(Rectangle().foregroundColor(.white).shadow(radius: 5))
+                }
+            }
+        }.ignoresSafeArea(.all, edges: .bottom)
+            .navigationTitle(viewModel.post.title)
             .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        self.isModifing = true
+                    }, label: {
+                        Image(systemName: "square.and.pencil")
+                    })
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        self.isDeleteAlert = true
+                    }, label: {
+                        Image(systemName: "trash")
+                            .foregroundColor(.red)
+                    })
+                }
+            }
+            .textFieldAlert(isPresented: $isModifyAlert) { () -> TextFieldAlert in
+                TextFieldAlert(title: "수정을 위한 비밀번호를 입력해주세요.", textFieldType: .single, action: viewModel.modifyPost, password: self.$viewModel.prevPassword, modifyString: self.$viewModel.deletePostTitle)
+            }
+            .textFieldAlert(isPresented: $isDeleteAlert) { () -> TextFieldAlert in
+                TextFieldAlert(title: "글 삭제", textFieldType: .delete, action: viewModel.deletePost, password: self.$viewModel.prevPassword, modifyString: self.$viewModel.deletePostTitle)
+            }
+            .toast(message: viewModel.errorMessage,
+                   isShowing: $viewModel.showErrorToast)
             .introspectTabBarController { (UITabBarController) in
                 UITabBarController.tabBar.isHidden = true
+            }
+            .onAppear {
+                self.viewModel.onAppear(id: id, viewCloseAction: {
+                    self.presentationMode.wrappedValue.dismiss()
+                })
             }
     }
 }
 
 struct PostView_Previews: PreviewProvider {
     static var previews: some View {
-        PostView()
+        PostView(id: "627cc1ac454d90856eca9edc")
     }
 }
--- Flower-iOS/View/CommunityView/Post/PostViewModel.swift
+++ Flower-iOS/View/CommunityView/Post/PostViewModel.swift
@@ -8,5 +8,71 @@
 import Foundation
 
 class PostViewModel: ObservableObject {
+    @Published var post = DetailPost.EMPTY
     
+    @Published var prevPassword: String? = ""
+    @Published var deletePostTitle: String? = ""
+    @Published var modifiedContent = ""
+    
+    @Published var showErrorToast = false
+    @Published var errorMessage = ""
+    
+    private let repository: CommunityRepository
+    private var viewCloseAction: () -> () = {}
+    
+    public init(repository: CommunityRepository = CommunityRepositoryImpl()) {
+        self.repository = repository
+    }
+    
+    public func onAppear(id: String, viewCloseAction: @escaping () -> ()) {
+        self.viewCloseAction = viewCloseAction
+        
+        self.repository.getDetailPost(id: id, completion: {
+            self.post = $0
+            self.modifiedContent = $0.content
+            self.deletePostTitle = $0.title
+        })
+    }
+    
+    public func modifyPost() {
+        guard let prevPassword = prevPassword else {
+            return
+        }
+        
+        var imageDatas = [Data]()
+        
+        for image in post.image {
+            repository.getImageData(id: image, completion: {
+                if $0 != nil {
+                    imageDatas.append($0!)
+                }
+            })
+        }
+        
+        self.repository.modifyPost(id: self.post.id, title: self.post.title, content: self.modifiedContent, images: imageDatas, nickname: self.post.nickname, password: prevPassword, completion: {
+            switch $0 {
+            case .success:
+                self.onAppear(id: self.post.id, viewCloseAction: self.viewCloseAction)
+            case let .failure(error: error):
+                self.showErrorToast = true
+                self.errorMessage = "비밀번호가 다릅니다."
+            }
+        })
+    }
+    
+    public func deletePost() {
+        guard let prevPassword = prevPassword else {
+            return
+        }
+
+        self.repository.deletePost(id: self.post.id, nickname: self.post.nickname, password: prevPassword, completion: {
+            switch $0 {
+            case .success:
+                self.viewCloseAction()
+            case let .failure(error: error):
+                self.showErrorToast = true
+                self.errorMessage = "비밀번호가 다릅니다."
+            }
+        })
+    }
 }
